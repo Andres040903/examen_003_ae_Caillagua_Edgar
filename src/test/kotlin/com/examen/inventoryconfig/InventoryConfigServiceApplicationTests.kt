3db1d28794deb9da4b0945076a5c5c842be3d179
Andres040903 <eacaillagua@puce.edu.ec>
2026-02-04 16:48:31 -0500

    init: inventory-config-service proyecto base

---
+++ src/test/kotlin/com/examen/inventoryconfig/InventoryConfigServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.examen.inventoryconfig
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InventoryConfigServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
